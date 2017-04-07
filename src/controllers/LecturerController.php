391edbc4408703a66739d67d346434bc9a3210a4
colinmck22 <b00092179@student.itb.ie>
2017-04-07 15:53:10 +0100

    silex update
    silex

---
+++ src/controllers/LecturerController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Itb\Controllers;
+
+class LecturerController
+{
+    private $app;
+
+    public function __construct(\Itb\WebApplication $app)
+    {
+        $this->app = $app;
+    }
+
+    // action for route:    /lecturer
+    // will we allow access to the lecturer home?
+    public function indexAction()
+    {
+        // test if 'username' stored in session ...
+        $username = $this->getAuthenticatedUserName();
+
+        // check we are authenticated --------
+        $isAuthenticated = (null != $username);
+        if(!$isAuthenticated){
+            // not authenticated, so redirect to LOGIN page
+            return $this->app->redirect('/login');
+        }
+
+        // store username into args array
+        $argsArray = [];
+
+        // render (draw) template
+        // ------------
+        $templateName = 'lecturer/index';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    // action for route:    /lecturerCodes
+    // will we allow access to the lecturerhome?
+    public function codesAction()
+    {
+        // test if 'username' stored in session ...
+        $username = $this->getAuthenticatedUserName();
+
+        // check we are authenticated --------
+        $isAuthenticated = (null != $username);
+        if(!$isAuthenticated){
+            // not authenticated, so redirect to LOGIN page
+            return $this->app->redirect('/login');
+        }
+
+        // store username into args array
+        $argsArray = [];
+
+        // render (draw) template
+        // ------------
+        $templateName = 'lecturer/codes';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    /**
+     * if user logged-in, THEN return user's username
+     * if user not logged-in THEN return 'null'
+     *
+     * @return null (or string username)
+     */
+    public function getAuthenticatedUserName()
+    {
+        // IF object (array) 'user' found with non-null value in 'session'
+        $user = $this->app['session']->get('user');
+
+        // if no such object in session, return NULL
+        if(null == $user){
+            return null;
+        }
+
+        // IF no value found in $user with key 'username' then return NULL
+        if (!isset($user['username'])){
+            return null;
+        }
+
+        // if we get here, we can return the value whose key is 'username'
+        return $user['username'];
+    }
+}
---
+++ src/controllers/MainController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Itb\Controllers;
+
+use Itb\WebApplication;
+
+class MainController
+{
+    private $app;
+
+    public function __construct(WebApplication $app)
+    {
+        $this->app = $app;
+    }
+
+    public function indexAction(){
+        $argsArray = [];
+        $templateName = 'index';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    public function publicAction(){
+        $argsArray = [];
+        $templateName = 'publicPage';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    public function aboutAction(){
+
+        $argsArray = [];
+        $templateName = 'about';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    public function listAction(){
+        // get reference to our repository
+        $userRepository = new \Itb\Model\UserRepository();
+        $users = $userRepository->getAll();
+
+        $argsArray = ['users' => $users];
+        $templateName = 'list';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    public function showAction($id)
+    {
+        // get reference to our repository
+        $userRepository = new \Itb\Model\UserRepository();
+        $users = $userRepository->getOneById($id);
+
+        if(null == $users){
+            $errorMessage = 'no user found with id = ' . $id;
+            $this->app->abort(404, $errorMessage);
+        }
+
+        $argsArray = ['users' => $users];
+        $templateName = 'show';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    public function showNoIdAction()
+    {
+        $errorMessage = 'you must provide an isbn for the show page (e.g. /show/123)';
+        // 400 - bad request
+        $this->app->abort(400, $errorMessage);
+    }
+
+    /*
+    public function getOneByUsername($username) {
+        $userRepository = new \Itb\Model\UserRepository();
+        $users = $userRepository->getAll();
+
+        foreach ($users as $user){
+            if($user == $username){
+                return $user;
+            }
+        }
+        return null;
+    }
+
+    public function canFindMatchingUsernameAndPassword($username, $password){
+        $user = $this->getOneByUsername($username);
+
+        // if no record has this username, return FALSE
+        if(null == $user){
+            return false;
+        }
+
+        // hashed correct password
+        $hashedStoredPassword = $user;
+
+        // return whether or not hash of input password matches stored hash
+        return password_verify($password, $hashedStoredPassword);
+    }
+
+//--------- helper public functions -------
+
+
+    public function isLoggedInFromSession()
+    {
+        $isLoggedIn = false;
+
+        // user is logged in if there is a 'user' entry in the SESSION superglobal
+        if(isset($_SESSION['user'])){
+            $isLoggedIn = true;
+        }
+
+        return $isLoggedIn;
+    }
+
+    public function usernameFromSession()
+    {
+        $username = '';
+
+        // extract username from SESSION superglobal
+        if (isset($_SESSION['user'])) {
+            $username = $_SESSION['user'];
+        }
+
+        return $username;
+    }
+   */
+}
