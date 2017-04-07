9838c03c228e55e8e8b7ae9ec14c6fd1783575c0
colinmck22 <b00092179@student.itb.ie>
2017-04-07 15:59:01 +0100

    Delete _links.php

--- templates/lecturer/_links.php
+++
@@ -1,9 +0,0 @@
-
-<ul>
-    <li>
-        <a href="/index.php?action=lecturerHome">lecturer - home</a>
-    </li>
-    <li>
-        <a href="/index.php?action=lecturerCodes">lecturer- codes</a>
-    </li>
-</ul>
