c5c89b2e2c233e8ef707dc1afd998b84815efa83
colinmck22 <b00092179@student.itb.ie>
2017-05-08 14:13:05 +0100

    Delete _links.php

--- templates/student/_links.php
+++
@@ -1,9 +0,0 @@
-
-<ul>
-    <li>
-        <a href="/index.php?action=studentHome">student - home</a>
-    </li>
-    <li>
-        <a href="/index.php?action=studentCodes">student- codes</a>
-    </li>
-</ul>
